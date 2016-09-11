7a08caa2b2e26d4f90c4fd9a52684a37263d6b84
Ji Woong <park.jiwoong@wustl.edu>
2016-09-11 11:37:46 -0500

    Added the index page

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ import webapp2
 import cgi
 import jinja2
 import os
+import re
 from google.appengine.ext import db
 
 # set up jinja
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),autoescape=True)
+# Using autoescape so I don't have to manually do CGI escape
 
-class Movie(db.Model):
-    title = db.StringProperty(required = True)
+class Post(db.Model): #This is where I am defining the Post (database).
+    subject = db.StringProperty(required = True)
+    content = db.TextProperty(required = True)
     created = db.DateTimeProperty(auto_now_add = True)
-    watched = db.BooleanProperty(required = True, default = False)
-    rating = db.StringProperty()
+    last_modified = db.DateTimeProperty(auto_now = True)
 
 class Handler(webapp2.RequestHandler):
     """ A base RequestHandler class for the site.
@@ -25,19 +27,29 @@ class Handler(webapp2.RequestHandler):
         self.error(error_code)
         self.response.write("Oops! Something went wrong.")
 
+class Index(Handler):
+    def get(self):
+        welcome_message = "Welcome! But you need to visit the " + "<a href='/blog'>blog</a>" + " page!"
+        self.response.write(welcome_message)
 
 class MainHandler(Handler):
-    """ Handles request to the main page.
+    """ Handles request to the main page of the blog. So '/blog' page
     """
 
     def get(self):
-        unwatched_movies = db.GqlQuery("SELECT * FROM Movie where watched = False")
+        unwatched_movies = db.GqlQuery("SELECT * FROM Post where watched = False")
         t = jinja_env.get_template("frontpage.html")
         response = t.render(
                         movies = unwatched_movies,
                         error = self.request.get("error"))
         self.response.write(response)
 
+# class Latest5Blog(Handler):
+#     def get(self):
+#         posts = db.GqlQuery("SELECT * FROM Post ")
+
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/',Index),
+    ('/blog', MainHandler)
 ], debug=True)
