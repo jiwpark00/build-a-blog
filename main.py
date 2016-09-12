ab704b5826324137fdd83028a3b423c362ae0acc
Ji Woong <park.jiwoong@wustl.edu>
2016-09-12 00:40:02 -0500

    Fixed the pesky error - thank goodness

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ template_dir = os.path.join(os.path.dirname(__file__), "templates")
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),autoescape=True)
 # Using autoescape so I don't have to manually do CGI escape
 
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
+
 class Post(db.Model): #This is where I am defining the Post (database).
     title = db.StringProperty(required = True)
     post = db.TextProperty(required = True)
@@ -19,10 +23,14 @@ class Handler(webapp2.RequestHandler):
     """ A base RequestHandler class for the site.
         The other handlers inherit form this one.
     """
-    def renderError(self, error_code):
-        """ Sends an HTTP error code and a generic "oops!" message to the client. """
-        self.error(error_code)
-        self.response.write("Oops! Something went wrong.")
+    def write(self, *a, **kw):
+        self.response.out.write(*a,**kw)
+
+    def render_str(self,template,**params):
+        return render_str(template,**params)
+
+    def render(self,template,**kw):
+        self.write(self.render_str(template,**kw))
 
 class Index(Handler):
     def get(self):
@@ -33,19 +41,15 @@ class MainHandler(Handler):
     """ Handles request to the main page of the blog. So '/blog' page
     """
     def get(self):
-        #posts = db.GqlQuery("SELECT * FROM Post ORDER BY created DESC LIMIT 5")
-        t = jinja_env.get_template("frontpage.html")
-        response = t.render()#posts = posts)
-        self.response.write(response)
+        posts = db.GqlQuery("SELECT * FROM Post ORDER BY created DESC LIMIT 5")
+        self.render('frontpage.html',posts=posts)
 
 class NewEntry(Handler):
     """ Handles request to the new post writing of the blog. So '/blog/newpost' page.
     """
 
     def get(self):
-        t = jinja_env.get_template("newpost.html")
-        response = t.render()
-        self.response.write(response)
+        self.render("newpost.html")
 
     def post(self):
         title = self.request.get("title")
@@ -56,17 +60,23 @@ class NewEntry(Handler):
             new_entry.put()
             self.redirect('/blog/%s' % str(new_entry.key().id()))
         else:
-            error = "be sure to enter title and post!"
-            t = jinja_env.get_template("newpost.html")
-            response = t.render(title = title, post = post, error = error)
-            self.response.write(response)
+            error = "Be sure to enter title and post!"
+            self.render("newpost.html",title=title,post=post,error=error)
 
-class ViewPostHandler(webapp2.RequestHandler):
+class ViewPostHandler(Handler):
     def get(self, id):
-        self.response.write(id)
+        entry = Post.get_by_id(int(id))
+
+        if not entry:
+            error = "<h3>Not a valid post ID! Return to <a href='/blog'>Main</a></h3>"
+            self.response.write(error)
+            return
+
+        self.render("permalink.html",entry=entry)
 
 app = webapp2.WSGIApplication([
     ('/',Index),
     ('/blog', MainHandler),
-    ('/blog/newpost', NewEntry), webapp2.Route('/blog/<id:\d+>', ViewPostHandler)],
+    ('/blog/newpost', NewEntry),
+    webapp2.Route('/blog/<id:\d+>', ViewPostHandler)],
     debug=True)
