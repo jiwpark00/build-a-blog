3eabe65318442f1c32f17659207b1d7c58aeaf35
Ji Woong <park.jiwoong@wustl.edu>
2016-09-11 21:28:16 -0500

    Making some progress

--- main.py
+++ main.py
@@ -11,19 +11,16 @@ jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),au
 # Using autoescape so I don't have to manually do CGI escape
 
 class Post(db.Model): #This is where I am defining the Post (database).
-    subject = db.StringProperty(required = True)
-    content = db.TextProperty(required = True)
+    title = db.StringProperty(required = True)
+    post = db.TextProperty(required = True)
     created = db.DateTimeProperty(auto_now_add = True)
-    last_modified = db.DateTimeProperty(auto_now = True)
 
 class Handler(webapp2.RequestHandler):
     """ A base RequestHandler class for the site.
         The other handlers inherit form this one.
     """
-
     def renderError(self, error_code):
         """ Sends an HTTP error code and a generic "oops!" message to the client. """
-
         self.error(error_code)
         self.response.write("Oops! Something went wrong.")
 
@@ -36,16 +33,40 @@ class MainHandler(Handler):
     """ Handles request to the main page of the blog. So '/blog' page
     """
     def get(self):
-        posts = db.GqlQuery("SELECT * FROM Post ORDER BY created DESC LIMIT 5")
+        #posts = db.GqlQuery("SELECT * FROM Post ORDER BY created DESC LIMIT 5")
         t = jinja_env.get_template("frontpage.html")
-        response = t.render(posts = posts)
+        response = t.render()#posts = posts)
         self.response.write(response)
 
 class NewEntry(Handler):
-    """ Handles request to the new post writing of the blog. So '/blog/newpost' page. """
+    """ Handles request to the new post writing of the blog. So '/blog/newpost' page.
+    """
+
+    def get(self):
+        t = jinja_env.get_template("newpost.html")
+        response = t.render()
+        self.response.write(response)
+
+    def post(self):
+        title = self.request.get("title")
+        post = self.request.get("post")
+
+        if title and post:
+            new_entry = Post(title = title, post = post)
+            new_entry.put()
+            self.redirect('/blog/%s' % str(new_entry.key().id()))
+        else:
+            error = "be sure to enter title and post!"
+            t = jinja_env.get_template("newpost.html")
+            response = t.render(title = title, post = post, error = error)
+            self.response.write(response)
+
+class ViewPostHandler(webapp2.RequestHandler):
+    def get(self, id):
+        self.response.write(id)
 
 app = webapp2.WSGIApplication([
     ('/',Index),
     ('/blog', MainHandler),
-    ('/blog/newpost', NewEntry)
-], debug=True)
+    ('/blog/newpost', NewEntry), webapp2.Route('/blog/<id:\d+>', ViewPostHandler)],
+    debug=True)
